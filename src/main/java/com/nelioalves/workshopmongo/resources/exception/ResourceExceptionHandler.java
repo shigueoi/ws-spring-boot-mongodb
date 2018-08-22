4326dcc978e834231b809f0c62adc31863c7a6bb
Shigueo <shigueo.isotani@gmail.com>
2018-08-21 23:03:13 -0300

    insercao usuario com post

--- src/main/java/com/nelioalves/workshopmongo/resources/exception/ResourceExceptionHandler.java
+++ src/main/java/com/nelioalves/workshopmongo/resources/exception/ResourceExceptionHandler.java
@@ -2,7 +2,6 @@ package com.nelioalves.workshopmongo.resources.exception;
 
 import javax.servlet.http.HttpServletRequest;
 
-import org.springframework.http.HttpRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
